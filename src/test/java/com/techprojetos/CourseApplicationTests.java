011549571b479e22479d3adebc7148c1edfb47c5
Ryan De Oliveira Lima <bryan@indracompany.com>
2022-03-08 11:35:48 -0300

    Project created

---
+++ src/test/java/com/techprojetos/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techprojetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
